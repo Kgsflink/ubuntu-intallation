2a7c9add7bf70d47bc88f7bff1ef57b35d62b537
Gopal <kgopalsahani8@gmail.com>
2022-05-15 15:36:09 +0000

    initial commit

--- ubuntu-installer.py
+++ ubuntu-installer.py
@@ -10,7 +10,7 @@ cmd = 'apt install git'
 os.system(cmd) 
 cmd = 'apt install wget '
 os.system(cmd)
-cmd = 'curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/repo-fix.sh > repo.sh && chmod +x repo.sh && bash repo.sh && pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu-xfce.sh -O ubuntu-xfce.sh && chmod +x ubuntu-xfce.sh && bash ubuntu-xfce.sh '
+cmd = 'curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/repo-fix.sh > repo.sh && chmod +x repo.sh && bash repo.sh && pkg update -y && pkg install wget curl proot tar -y && wget https://github.com/devilboy66/ubuntu-permission-/blob/master/ubuntu-xfce.sh -O ubuntu-xfce.sh && chmod +x ubuntu-xfce.sh && bash ubuntu-xfce.sh '
 os.system(cmd)
 cmd = 'apt autoremove python'
 os.system(cmd)
